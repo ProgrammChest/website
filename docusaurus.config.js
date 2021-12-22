71d460cc9fee31eb5b8078ad6cd73b1a92253e56
CodeDoctorDE <codedoctor@linwood.dev>
2021-12-22 07:47:52 +0100

    update dependencies

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ProgrammChest, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
